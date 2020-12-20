a78e3c9ff2fbd1fc09d899e741d8404057559d37
jonthwright <jonthwright@gmail.com>
2020-12-20 06:00:02 +0000

    Update 20.12 #2
    Completed: Day 13 part 1
    TODO: Day 13 part 2

---
+++ day_13_-_shuttle_search/part_1.py
@@ -0,0 +1,16 @@
+def solution(inputs):
+	start = int(inputs[0])
+	buses = [int(bus) for bus in inputs[1].split(',') if bus != 'x']
+	waits = [(bus, bus - (start % bus)) for bus in buses]
+
+	target_bus = min(waits, key=lambda x: x[1])
+
+	return target_bus[0] * target_bus[1]
+
+def main():	
+	with open('input_file.txt', 'r') as f:
+		inputs = [e for e in f.read().strip().splitlines()]
+	print(solution(inputs))
+
+if __name__ == '__main__':
+	main()
