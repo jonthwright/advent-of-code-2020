633cda68ce3e6c6bf2d4c6cdad5ccb8a8eaef471
jonthwright <jonthwright@gmail.com>
2020-12-22 15:17:28 +0000

    Delete part_2.py

--- day_20_-_jurassic_jigsaw/part_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def solution(elements):
-	pass
-
-if __name__ == '__main__':
-	with open('input_file.txt', 'r') as f:
-		inputs = f.read().split("\n\n")
-	print(f'Answer : {solution(inputs)}')
